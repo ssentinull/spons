f53edefd4e9f75c5c799d09d5c59a3a0e9c17b93
IbnuAhsani <ibnu.muhari@gmail.com>
2019-03-29 11:20:14 +0700

    create grant model and migration

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    public function grants(){
+        return $this->hasMany('App\Grant');
+    }
 }
---
+++ database/migrations/2019_03_29_041145_create_grants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGrantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type');
+            $table->integer('nominal_amount');
+            $table->longText('descriptive_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grants');
+    }
+}
