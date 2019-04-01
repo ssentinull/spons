f6775d7d32b0fc41db9cf1db42a0d9eea3b3ca08
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-01 21:12:21 +0700

    create event model and migration

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function events(){
+        return $this->hasMany('App\Event');
+    }
 }
---
+++ database/migrations/2019_04_01_140257_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->date('date');
+            $table->string('location');
+            // Ex; Seminar, Conference, Concert, etc.
+            $table->integer('type');
+            // Ex; Technology, Environment, Social
+            $table->integer('category');
+            $table->integer('student_id')->unsigned();
+            $table->integer('document_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
