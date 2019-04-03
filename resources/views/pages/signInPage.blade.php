e98341a6ab47d1b8fafceebbf9aaae8031e9a3f5
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-03 12:45:13 +0700

    create landing and sign in page

---
+++ resources/views/pages/signInPage.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{config('app.name')}}</h5>
+            <form>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-success">Sign In</button>
+                <a href="#" class="btn btn-outline-success">Register</a>
+            </form>
+        </div>
+    </div>
+@endsection
