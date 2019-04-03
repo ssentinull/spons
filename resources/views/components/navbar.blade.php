e185fd05c3cc5d149b021b8890a312b290ae4ee2
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-03 11:20:31 +0700

    create navigation bar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Events</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Companies</a>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-success my-2 my-sm-0" type="submit">Sign Up</button>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign In</button>
+      </li>
+    </ul>
+  </div>
+</nav>
