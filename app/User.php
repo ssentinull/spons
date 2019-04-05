29e5ecb1dad9d24b3f210c6f15e163b79ecb65c6
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-05 16:26:34 +0700

    add User table fillable fields

--- app/User.php
+++ app/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'password', 'name', 'city', 'description',
+        'role', 'dob', 'major', 'faculty', 'university', 'address',
     ];
 
     /**
