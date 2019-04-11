4ebc1fa63242f2a0fad7e952661402c41eacaa44
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-11 10:18:34 +0700

    create student sidebar component

---
+++ resources/views/components/studentSidebar.blade.php
@@ -0,0 +1,16 @@
+<!-- Side navigation -->
+<div class="sidenav">
+    <div class="image-cropper" href="https://placeholder.com">
+        <img src="https://via.placeholder.com/360">
+    </div>
+    <a class="disabled" href="#">Student's Name</a>
+    <a class="disabled" href="#">Major</a>
+    <a class="disabled" href="#">Faculty</a>
+    <a class="disabled" href="#">University</a>
+    <a class="disabled" href="#">Description</a>
+    <hr>
+    <a class="non-disabled" href="#">Events</a>
+    <a class="non-disabled" href="#">Sponsorship Requests</a>
+    <a class="non-disabled" href="#">Transactions</a>
+    <hr>
+</div>
