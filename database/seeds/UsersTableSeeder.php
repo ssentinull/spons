85912d992f851842afb0af880cabc60b599c2d6f
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-16 12:43:41 +0700

    feat: default Student and Company account to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
+
+        DB::table('users')->insert([
+            'email' => 'ibnu.muhari@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '12345678',
+            'name' => 'Ibnu Ahsani',
+            'city' => 'Jakarta',
+            'description' => 'Student of Informatics, Padjadjaran University',
+            'role' => Constant::ROLE_STUDENT,
+            'dob' => $faker->date,
+            'major' => 'Informatics',
+            'faculty' => 'Department of Computer Science',
+            'university' => 'Padjadjaran University',
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'hello.world@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '12345678',
+            'name' => 'Hello World',
+            'city' => 'Bandung',
+            'description' => 'Company that\'s located in Bandung',
+            'role' => Constant::ROLE_COMPANY,
+            'address' => $faker->address,
+            'remember_token' => Str::random(10),
+        ]);
+
         factory(App\User::class, 'student', 2)->create();
         factory(App\User::class, 'company', 5)->create();
     }
