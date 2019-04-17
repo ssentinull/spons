2f20c083a0b9b0d5c93fe883503048b57f334002
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-17 21:46:07 +0700

    fix: unable to display only one event in Events page

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -6,22 +6,31 @@
 <div class="container">
     <table class="table table-borderless">
        <tbody>
-           @for ($i = 0; $i < $events->count(); $i+=2)
-            <tr>
+           @if ($events->count() > 1)
+                @for ($i = 0; $i < $events->count(); $i+=2)
+                    <tr>
+                        <td>
+                            <div class="card">
+                                <div class="card-header">{{$events[$i]->name}}</div>
+                                <div class="card-body">{{$events[$i]->location}}</div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="card">
+                                <div class="card-header">{{$events[$i + 1]->name}}</div>
+                                <div class="card-body">{{$events[$i + 1]->location}}</div>
+                            </div>
+                        </td>
+                    </tr>
+                @endfor
+            @else
                 <td>
                     <div class="card">
-                        <div class="card-header">{{$events[$i]->name}}</div>
-                        <div class="card-body">{{$events[$i]->location}}</div>
+                        <div class="card-header">{{$events[0]->name}}</div>
+                        <div class="card-body">{{$events[0]->location}}</div>
                     </div>
                 </td>
-                <td>
-                    <div class="card">
-                        <div class="card-header">{{$events[$i + 1]->name}}</div>
-                        <div class="card-body">{{$events[$i + 1]->location}}</div>
-                    </div>
-                </td>
-            </tr>
-           @endfor
+            @endif
        </tbody>
     </table>
     {{ $events->links() }}
