a6828c395da96d9508c85768ac53aa2380f34c3c
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-17 21:49:00 +0700

    refactor: rename Event_Type model to EventType

--- app/EventType.php
+++ app/EventType.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event_Type extends Model
+class EventType extends Model
 {
     /**
      * The attributes that are mass assignable.
