281c61b30ef59850199cd6a07d4e707e5af80743
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-27 11:03:42 +0700

    fix: add fillable fields to grant model

--- app/Grant.php
+++ app/Grant.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grant extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nominal_amount', 'descriptive_amount', 'grant_types_id', 'user_id'
+    ];
+
     public function company(){
         return $this->belongsTo('App\User');
     }
