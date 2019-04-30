60d9ea41ec86569f8c157dd6a2191d1cdafcdce3
IbnuAhsani <ibnu.muhari@gmail.com>
2019-04-30 15:48:50 +0700

    feat: color the typography in companies page

--- resources/views/pages/companies.blade.php
+++ resources/views/pages/companies.blade.php
@@ -10,24 +10,24 @@
 
 @section('content')
 <div class="container">
-    <div>
-        <center>
-            <h1 style="margin-left:5px; color: #0E8C7F;">Companies that are listed in our platform</h1>
-        </center>
+    <div class="row justify-content-center top-buffer">
+        <h2 style="margin-left:5px;"><span style="color: #0E8C7F; font-size:40px">Companies</span> that are listed in our platform</h2>
     </div>
-    <div class="row justify-content-center">
-        <div class="row">
-            <p style="color: #0E8C7F;">Showing {{$firstCompanyIndex + 5}} out of {{$companies->total()}}</p>
+    <div class="row top-buffer">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+            <h4 style="color: #0E8C7F;">Showing {{$firstCompanyIndex + 5}} out of {{$companies->total()}}</h4>
         </div>
-        <div class="row">
-            <div class="prodictrow">
+        <div class="col-md-6"></div>
+    </div>
+    <div class="row">
+        <div class="prodictrow">
                 @foreach($companies as $company)
                     @include('components.eventsCard')
                 @endforeach
-            </div>
         </div>
     </div>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center top-buffer">
         {{ $companies->links() }}
     </div>
 </div>
