063bc1a7b4f0738d7155d7dd3ac0737cff3bacc7
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-05 16:54:12 +0700

    feat: company sponsorship status constant variables

--- app/Constant.php
+++ app/Constant.php
@@ -3,6 +3,8 @@
 namespace App;
 
 class Constant{
+    const COMPANY_STATUS_AVAILABLE = 1;
+    const COMPANY_STATUS_UNAVAILABLE = 0;
     const ROLE_STUDENT_INDIVIDUAL = 1;
     const ROLE_STUDENT_ORGANIZATION = 2;
     const ROLE_COMPANY = 3;
