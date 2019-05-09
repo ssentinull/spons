823b4a0fdc4d722e0a9a451218149e300678df6f
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-10 00:24:18 +0700

    refactor: sidebar html & css

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,63 +1,81 @@
-<center>
 <div class="sidenav">
-    <div class="image-cropper" href="https://placeholder.com">
-        <img src="https://via.placeholder.com/360">
-    </div>
-    <a class="disabled" href="#">{{Auth::user()->name}}</a> 
-    @if (Auth::user()-> role == Constant::ROLE_COMPANY)
-        <!-- <a class="disabled" href="#">{{$userData->established_in}}</a> -->
-        <a class="disabled" href="#">Status</a> <br>
-        <a class="disabled" href="#">{{$userData->address}}</a>
-        <!-- <a class="disabled" href="#">{{$userData->description}}</a> -->
-    @elseif(Auth::user()-> role == Constant::ROLE_STUDENT_INDIVIDUAL)
-        <!-- <a class="disabled" href="#">{{$userData->dob}}</a> -->
-        <a class="disabled" href="#" style="color: black;">Status</a><br>
-        <!-- <a class="disabled" href="#">{{$userData->city}}</a> -->
-        <!-- <a class="disabled" href="#">{{$userData->major}}</a> -->
-        <!-- <a class="disabled" href="#">{{$userData->faculty}}</a> -->
-        <a class="disabled" href="#">{{$userData->university}}</a>
-    @elseif(Auth::user()-> role == Constant::ROLE_STUDENT_ORGANIZATION)
-        <!-- <a class="disabled" href="#">{{$userData->established_in}}</a> -->
-        <a class="disabled" href="#">Status</a><br>
-        <!-- <a class="disabled" href="#">{{$userData->major}}</a> -->
-        <!-- <a class="disabled" href="#">{{$userData->university}}</a> -->
-        <a class="disabled" href="#">{{$userData->address}}</a>
-        <!-- <a class="disabled" href="#">{{$userData->description}}</a> -->
-    @endif
-    <hr>
-    <a class="non-disabled" href="#">Events</a>
-    <a class="non-disabled" href="{{url('/request')}}">Sponsorship Requests</a>
-    <a class="non-disabled" href="#">Transactions</a>
-    <hr>
+    <center>
+        <div class="image-cropper" href="https://placeholder.com">
+            <img src="https://via.placeholder.com/360">
+        </div>
 
-    <div class="editp">
-    <button onclick="openForm()">Edit Profile</button>
- 
-</center> 
-    <div class="form-popup" id="myForm">
-  <form action="#" class="form-container"> 
-    <h3>Edit Profile</h3>
+        <a class="disabled" href="#">{{Auth::user()->name}}</a>
+        @if (Auth::user()-> role == Constant::ROLE_COMPANY)
+            <a class="disabled">{{$userData->established_in}}</a>
+            @if ($userData->status == Constant::COMPANY_STATUS_AVAILABLE)
+                <a class="disabled" style="color: #3CB371">Available for Sponsorship</a>
+            @else
+                <a class="disabled" style="color: #B22222">Unavailable for Sponsorship</a>
+            @endif
+            <a class="disabled">{{$userData->address}}</a>
+        @elseif(Auth::user()-> role == Constant::ROLE_STUDENT_INDIVIDUAL)
+            <a class="disabled">{{$userData->major}}</a>
+            <a class="disabled">{{$userData->faculty}}</a>
+            <a class="disabled">{{$userData->university}}</a>
+        @elseif(Auth::user()-> role == Constant::ROLE_STUDENT_ORGANIZATION)
+            <a class="disabled">{{$userData->major}}</a>
+            <a class="disabled">{{$userData->university}}</a>
+            <a class="disabled">{{$userData->address}}</a>
+        @endif
+        <hr>
 
-    <div class="image-cropper" href="https://placeholder.com">
-        <img src="https://via.placeholder.com/360">
-    </div>
-    <label for="name"><b>Name</b></label>
-    <input type="text" placeholder="Enter your new Name" name="name" required>
+        <a class="non-disabled" href="#">Events</a>
+        <a class="non-disabled" href="{{url('/request')}}">Sponsorship Requests</a>
+        <a class="non-disabled" href="#">Transactions</a>
+        <hr>
 
-    <label for="address"><b>Address</b></label>
-    <input type="text" placeholder="Enter your new address" name="address" required>
-
-    <label for="city"><b>City</b></label>
-    <input type="text" placeholder="Enter your new city" name="city" required>
-
-    <label for="city"><b>Description</b></label><br>    
-    <textarea placeholder="Enter your new Description" name="desc" required></textarea>
-
-    <button type="submit" class="btn">Update</button><br>
-    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-  </form>
+        <button type="button" class="green-button" data-toggle="modal" data-target="#basicExampleModal">Edit Profile</button>
+        {{-- <button onclick="openForm()">Edit Profile</button> --}}
+    </center>
 </div>
 
+<div class="form-popup" id="myForm">
+    <form action="#" class="form-container">
+        <h3>Edit Profile</h3>
 
+        <div class="image-cropper" href="https://placeholder.com">
+            <img src="https://via.placeholder.com/360">
+        </div>
+        <label for="name"><b>Name</b></label>
+        <input type="text" placeholder="Enter your new Name" name="name" required>
+
+        <label for="address"><b>Address</b></label>
+        <input type="text" placeholder="Enter your new address" name="address" required>
+
+        <label for="city"><b>City</b></label>
+        <input type="text" placeholder="Enter your new city" name="city" required>
+
+        <label for="city"><b>Description</b></label><br>
+        <textarea placeholder="Enter your new Description" name="desc" required></textarea>
+
+        <button type="submit" class="btn">Update</button><br>
+        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    </form>
+</div>
+
+<!-- events picker modal -->
+<div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Select Events</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form role="form" method="POST" action="{{ route('studentRequestsSponsorship') }}">
+                @csrf
+                <div class="modal-body">
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="green-button">Apply for Sponsorship</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
