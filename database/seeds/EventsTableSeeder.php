1d833a1c5321fa03bd2bb7d68f5296f10cb7d956
IbnuAhsani <ibnu.muhari@gmail.com>
2019-10-30 09:49:40 +0700

    feat: update events & users table seeder data

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class EventsTableSeeder extends Seeder
 {
@@ -11,6 +12,96 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Event::class, 50)->create();
+        $faker = Faker::create();
+
+        DB::table('events')->insert([
+            'name' => 'Seminar Nasional iFest',
+            'type' => 1,
+            'category' => 1,
+            'description' => 'Seminar Nasional iFest merupakan rangkaian dari kegiatan besar Informatics Festival di jurusan Teknik Informatika Universitas Padjadjaran, seminar ini memiliki topik beragam disesuaikan dengan tema festival. Saat ini topik seminar adalah mempelajari Big Data dalam rangka menyambut Revolusi Industry 4.0, peserta memiliki beberapa golongan; mahasiswa, orang luar, dan pemakalah dengan harga tiket masuk yang berbeda-beda tiap golongannya',
+            'date' => '2019-12-01',
+            'location' => 'Jatinangor',
+            'user_id' => 2
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'Bunkasai Festival',
+            'type' => 2,
+            'category' => 3,
+            'description' => 'Acara tahunan yang diselenggarakan oleh sebagian besar sekolah di Jepang , dari  Taman Kanak- kanak dan universitas di mana siswa menampilkan prestasi sehari-hari mereka. Orang yang ingin masuk sekolah atau yang tertarik dengan sekolah mungkin datang untuk melihat apa saja kegiatan yang sekolah dan suasana seperti. Orang tua juga mungkin ingin melihat jenis pekerjaan yang dilakukan anak-anak mereka',
+            'date' => '2019-10-11',
+            'location' => 'Bandung',
+            'user_id' => 7
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'Entrepreneurship Chall As Ro4',
+            'type' => 3,
+            'category' => 2,
+            'description' => 'Komunitas Tangan Di Atas (TDA) kembali menggelar acara Pesta Wirausaha Nasional 2019 yang akan dihadiri oleh beragam pemangku kepentingan kewirausahaan di Indonesia. Acara Pesta Wirausaha Nasional yang ke- 11 ini digelar pada 25-27 Januari 2019. Bertemakan “Kolaboraksi”, agenda rutin setiap tahun ini akan diadakan di E-Covention Centre Ancol, Jakarta',
+            'date' => '2019-11-09',
+            'location' => 'Jatinangor',
+            'user_id' => 6
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'October Food Vaganza',
+            'type' => 3,
+            'category' => 2,
+            'description' => 'Membahas tentang wawasan kuliner di Bandung. Buat yang tertarik akan makanan khas bandung dan mempelajarinya datang aja ke Bandung Indah Plaza. Acara ini akan hadir selama bulan Oktober dengan berbagai macam kegiatan.',
+            'date' => '2019-12-01',
+            'location' => 'Bandung',
+            'user_id' => 7
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'DCDC Rock N’Semble',
+            'type' => 2,
+            'category' => 5,
+            'description' => 'Warga Bandung kangen nonton penampilan Rosemary? Minggu ini ada nih DCDC Rock N’Semble : Rosemary Ki Ageng Ganjur. Selain ada penampilan Rosemary, akan ada juga penampilan dari Olegun Gobs, Stand Here Alone, Lowdick, Maw & Wang, dan masih banyak lagi. Acara ini diselenggarakan di Lapangan Pussenif pada tanggal 19 Oktober 2019.',
+            'date' => '2019-10-26',
+            'location' => 'Bandung',
+            'user_id' => 8
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'Diaspora 2019',
+            'type' => 4,
+            'category' => 5,
+            'description' => 'Warga Bandung ingin membuka usaha? Atau sedang cari kerja? Wargi Bandung wajib datang ke acara ini nih. Di acara ini akan ada talkshow, career expo, startup expo, dan beragam acara seru lainnya. Selain itu, akan ada juga penampilan spesial dari Rizky Febian loh. Acara ini digelar di Telkom University Convention Hall pada tanggal 17 Oktober 2019.',
+            'date' => '2019-10-28',
+            'location' => 'Bandung',
+            'user_id' => 9
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'European Higher Education Fair (EHEF)',
+            'type' => 1,
+            'category' => 1,
+            'description' => 'Bermimpi Melanjutkan Studi ke Eropa? Jangan lewatkan EHEF 2019, pameran pendidikan tinggi Eropa terbesar di Indonesia yang paling dinanti-nanti.',
+            'date' => '2019-10-30',
+            'location' => 'Bandung',
+            'user_id' => 8
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'Colosseum Food Festival 2019',
+            'type' => 2,
+            'category' => 3,
+            'description' => 'Ada satu lagi nih acara kuliner di Bandung minggu ini. Di acara persembahan ITHB ini terdapat banyak tenant kuliner. Colosseum Food Festival ini diselenggarakan pada tanggal 19 Oktober 2019 di halaman depan ITHB, Jl Dipati Ukur',
+            'date' => '2019-12-01',
+            'location' => 'Jatinangor',
+            'user_id' => 9
+        ]);
+
+        DB::table('events')->insert([
+            'name' => 'Transmart Gowes',
+            'type' => 4,
+            'category' => 5,
+            'description' => 'Warga Bandung yang hobi gowes, bisa ikutan acara ini nih. Sambil gowes, Wargi Bandung juga bisa mendapatkan berbagai macam hadiah di Transmart Gowes ini. Acara ini diadakan di Transmart Trans Studio Mall Bandung, tanggal 20 Oktober 2019',
+            'date' => '2019-12-01',
+            'location' => 'jatinangor',
+            'user_id' => 8
+        ]);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,7 +23,12 @@ class UsersTableSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
-        factory(App\StudentIndividual::class)->create([
+        DB::table('student_individuals')->insert([
+            'dob' => '1997-05-17',
+            'city' => 'Jakarta',
+            'major' => 'Informatics',
+            'faculty' => 'Department of Computer Science',
+            'university' => 'Padjadjaran University',
             'user_id' => 1,
         ]);
 
@@ -36,34 +41,14 @@ class UsersTableSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
-        factory(App\StudentOrganization::class)->create([
+        DB::table('student_organizations')->insert([
+            'established_in' => '01-01-2012',
+            'address' => 'Jatinangor',
+            'major' => 'Informatics',
+            'university' => 'Padjadjaran University',
+            'description' => 'Merupakan sebuah himpunan yang berada di universitas padjadjaran, beridiri pada tahaun 2012, dan memiliki beberapa proker dan eksistensi yang bagus di dunia informatika Indonesia baik secara akademik maupun sosial',
             'user_id' => 2,
         ]);
-
-        DB::table('users')->insert([
-            'email' => 'hello.world@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('12345678'),
-            'name' => 'Astra Jaya',
-            'role' => Constant::ROLE_COMPANY,
-            'remember_token' => Str::random(10),
-        ]);
-
-        factory(App\Company::class)->create([
-            'user_id' => 3,
-        ]);
-
-        factory(App\User::class, 'studentIndividual', 2)->create()->each(function($user){
-            factory(App\StudentIndividual::class)->create(['user_id' => $user->id]);
-        });
-
-        factory(App\User::class, 'studentOrganization', 2)->create()->each(function($user){
-            factory(App\StudentOrganization::class)->create(['user_id' => $user->id]);
-        });
-
-        factory(App\User::class, 'company', 10)->create()->each(function($user){
-            factory(App\Company::class)->create(['user_id' => $user->id]);
-        });
 
         DB::table('users')->insert([
             'email' => 'AstraMotor@astra.com',
@@ -79,7 +64,115 @@ class UsersTableSeeder extends Seeder
             'address' => 'Jakarta Utara',
             'description' => 'PT Astra International - Honda adalah salah satu perusahaan Astra International yang bergerak di bidang operations dengan produk sepeda motor Honda. Didirikan pada tahun 1970, dengan nama Honda Division, Astra Motor dahulu merupakan main distributor sepeda motor Honda.',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 18,
+            'user_id' => 3,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'AhsanNurrijal@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'Ahsan Nurrijal',
+            'role' => Constant::ROLE_STUDENT_INDIVIDUAL,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_individuals')->insert([
+            'dob' => '02-04-1998',
+            'city' => 'Jatinangor',
+            'major' => 'Informatics',
+            'faculty' => 'Department of Computer Science',
+            'university' => 'Padjadjaran University',
+            'user_id' => 4,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'FauziFaruq@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'Fauzi Faruq',
+            'role' => Constant::ROLE_STUDENT_INDIVIDUAL,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_individuals')->insert([
+            'dob' => '05-04-1998',
+            'city' => 'jatinangor',
+            'major' => 'informatics',
+            'faculty' => 'Department of Computer Science',
+            'university' => 'Padjadjaran University',
+            'user_id' => 5,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'phyITB@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'Fisika ITB',
+            'role' => Constant::ROLE_STUDENT_ORGANIZATION,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_organizations')->insert([
+            'established_in' => '01-01-1951',
+            'address' => 'jl.Raya bandung 176',
+            'major' => 'Physics',
+            'university' => 'Bandung Institute of Tehcnology',
+            'description' => 'Program Studi (Prodi) Fisika, Fakultas Matematika dan Ilmu Pengetahuan Alam, Institut Teknologi Bandung (ITB) terletak di dalam kompleks Kampus ITB di Jalan Ganesa 10, Bandung 40132. Di antara jurusan-jurusan Fisika di Indonesia, Prodi Fisika adalah lembaga pendidikan dan penelitian fisika yang tertua. Awalnya, Prodi Fisika berdiri pada tahun 1948 sebagai bagian dari Fakulteit van Exacte Wetenschap dari Nood Universiteit van Jakarta. Kemudian fakultas tersebut berubah nama menjadi Faculteit van Wiskunde en Natuur Wetenschap. Setelah pengakuan kemerdekaan Indonesia oleh Belanda pada akhir 1949 nama fakultas tersebut diubah menjadi Fakultas Ilmu Pasti dan Ilmu Alam (FIPIA) dan merupakan bagian dari Universitas Indonesia. Pada tahun 1959 mendiang Presiden Soekarno meresmikan berdirinya Institut Teknologi Bandung dengan membawahi dua fakultas, yaitu Fakultas Teknik dan FIPIA. Sekarang FMIPA (Fakultas Matematika dan Ilmu Pengetahuan Alam) menggantikan nama FIPIA',
+            'user_id' => 6,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'himade@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'Himade FIB Unpad',
+            'role' => Constant::ROLE_STUDENT_ORGANIZATION,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_organizations')->insert([
+            'established_in' => '01-01-1951',
+            'address' => 'Jln. Raya Bandung-Sumedang Km. 21. Jatinangor, Kab. Sumedang',
+            'major' => 'Japanese Literature',
+            'university' => 'Padjadjaran University',
+            'description' => 'Kabinet Fuurinkazan merupakan nama kabinet Badan Pengurus Harian Himade S1 Unpad periode 2018-2019. Furinkazan diambil dari nama strategi perang yang digunakan daimyo Shingen Takeda. Keempat elemen yang digambarkan pada strategi tersebut adalah angin, hutan, api, dan gunung, yang memiliki arti Himade yang secepat bagai angin, setenang hutan, bersemangat berapi-api, dan kokoh seperti gunung.',
+            'user_id' => 7,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'himatika@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'Himatika FMIPA Unpad',
+            'role' => Constant::ROLE_STUDENT_ORGANIZATION,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_organizations')->insert([
+            'established_in' => '10-11-1960',
+            'address' => 'Jln. Raya Bandung-Sumedang Km. 21. Jatinangor, Kab. Sumedang',
+            'major' => 'Mathematics',
+            'university' => 'Padjadjaran University',
+            'description' => 'Himpunan Mahasiswa Matematika Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Padjadjaran atau biasa disebut Himatika FMIPA Unpad adalah organisasi mahasiswa tingkat sarjana (S1) yang berperan dalam mewadahi aspirasi dan kreatifitas Mahasiswa Matematika FMIPA Unpad. Himatika FMIPA Unpad didirikan pada 10 November 1960 di Bandung. Himatika FMIPA Unpad memiliki tiga Badan Kelengkapan (BK), yakni Pengurus Harian (PH), Majelis Perwakilan Anggota (MPA), dan Musyawarah Besar (Mubes) . Masing-masing Badan Kelengkapan tersebut memiliki peranannya masing-masing dalam siklus kehidupan Himatika FMIPA Unpad.',
+            'user_id' => 8,
+        ]);
+
+        DB::table('users')->insert([
+            'email' => 'bemkemafpik@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'name' => 'BEM KEMA FPIK Unpad',
+            'role' => Constant::ROLE_STUDENT_ORGANIZATION,
+            'remember_token' => Str::random(10),
+        ]);
+
+        DB::table('student_organizations')->insert([
+            'established_in' => '10-11-1960',
+            'address' => 'Jln. Raya Bandung-Sumedang Km. 21. Jatinangor, Kab. Sumedang',
+            'major' => 'Marine Science',
+            'university' => 'Padjadjaran University',
+            'description' => 'Badan Eksekutif Mahasiswa, Keluarga Mahasiswa Fakultas Perikanan dan Ilmu Kelautan Universitas Padjadjaran yang selanjutnya disingkat BEM Kema FPIK Unpad adalah lembaga eksekutif tertinggi di lingkungan FPIK Unpad.',
+            'user_id' => 9,
         ]);
 
         DB::table('users')->insert([
@@ -96,7 +189,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Jakarta',
             'description' => 'PT Telekomunikasi Indonesia Tbk, biasa disebut Telkom Indonesia atau Telkom saja adalah perusahaan informasi dan komunikasi serta penyedia jasa dan jaringan telekomunikasi secara lengkap di Indonesia',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 19,
+            'user_id' => 10,
         ]);
 
         DB::table('users')->insert([
@@ -113,7 +206,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Jakarta Pusat',
             'description' => 'Sejak 1 Agustus 1992 berdasarkan Undang-Undang Perbankan No. 7 tahun 1992 dan Peraturan Pemerintah RI No. 21 tahun 1992 status BRI berubah menjadi perseroan terbatas. Kepemilikan BRI saat itu masih 100% di tangan Pemerintah Republik Indonesia. Pada tahun 2003, Pemerintah Indonesia memutuskan untuk menjual 30% saham bank ini, sehingga menjadi perusahaan publik dengan nama resmi PT. Bank Rakyat Indonesia (Persero) Tbk., yang masih digunakan sampai dengan saat ini. ',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 20,
+            'user_id' => 11,
         ]);
 
         DB::table('users')->insert([
@@ -130,7 +223,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Jakarta Pusat',
             'description' => 'Academia.edu adalah situs jejaring sosial bagi akademisi. Serambi ini dapat digunakan untukberbagi dokumen, memantau dampaknya,dan mengikuti penelitian dalam bidang tertentu. Academia.edu diluncurkan pada bulan September 2008 oleh Richard Price dengan suntikan dana sebesar US$600.000 dari Spark Ventures, BrentHoberman, dan investor lainnya. ',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 21,
+            'user_id' => 12,
         ]);
 
         DB::table('users')->insert([
@@ -147,7 +240,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Bandung',
             'description' => 'Chunck Corporation adalah sebuah perusahaan kopi dan jaringan kedai kopi global asal Amerika Serikat yang berkantor pusat di Bandung. Mengambil referensi cita rasa kopi gaya budaya barat.',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 22,
+            'user_id' => 13,
         ]);
 
         DB::table('users')->insert([
@@ -164,7 +257,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Jl. Kemang Selatan I No. M5 B 6 2, RT.5/RW.3, Bangka, Kec. Mampang Prpt., Kota Jakarta Selatan',
             'description' => 'Lendiglob, startup yang bergerak di bidang kecerdasan buatan (AI), meresmikan produk baru Platform untuk membantu developer startup miliki teknologi sendiri. Tidak hanya untuk startup, platform ini juga disasar untuk developer dari perusahaan skala besar.',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 23,
+            'user_id' => 14,
         ]);
 
         DB::table('users')->insert([
@@ -178,10 +271,10 @@ class UsersTableSeeder extends Seeder
 
         DB::table('companies')->insert([
             'established_in' => '01-01-1987',
-            'address' => 'Jl. Kemang Raya No.54, RT.8/RW.2, Bangka, Kec. Mampang Prpt., Kota Jakarta Selatan',
+            'address' => 'Jl. Kemang Raya No.54, Kota Jakarta Selatan',
             'description' => 'Kata.ai, startup yang bergerak di bidang kecerdasan buatan (AI), meresmikan produk baru Kata Bot Platform untuk membantu developer startup miliki chatbot sendiri. Tidak hanya untuk startup, platform ini juga disasar untuk developer dari perusahaan skala besar.',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 24,
+            'user_id' => 15,
         ]);
 
         DB::table('users')->insert([
@@ -198,7 +291,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Bandung',
             'description' => 'Baskin-Robbins Adalah perusahaan ritel asal Amerika yang bergerak di bidang es krim and kue. Berbasis di Canton, Massachusetts, didirikan pada tahun 1945 oleh Burt Baskin dan Irv Robbins di Glendale',
             'status' => $faker->randomElement($array = array(0, 1)),
-            'user_id' => 25,
+            'user_id' => 16,
         ]);
     }
 }
